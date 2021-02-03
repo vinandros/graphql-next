bc0a5b421b4559a1e72272e3c8952166631de4f7
vinandros <vinandros@gmail.com>
2021-02-03 14:55:43 -0600

    basic apollo structure

---
+++ backend/db/schema.js
@@ -0,0 +1,5 @@
+const { gql } = require("apollo-server");
+// Schema
+const typeDefs = gql``;
+
+module.exports = typeDefs;
---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./db/schema");
+const resolvers = require("./db/resolvers");
+
+// Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server running on port ${url}`);
+});
